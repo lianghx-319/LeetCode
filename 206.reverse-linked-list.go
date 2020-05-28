fc4bba13110f71f779326ee97eb82a0b970cb17f
Han <xsytby1112@gmail.com>
2020-05-28 17:44:16 +0800

    feat: 反转链表1

---
+++ 206.reverse-linked-list.go
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=206 lang=golang
+ *
+ * [206] Reverse Linked List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	cur := head
+	for cur != nil {
+		cur.Next, pre, cur = pre, cur, cur.Next
+	}
+	return pre
+}
+
+// @lc code=end
