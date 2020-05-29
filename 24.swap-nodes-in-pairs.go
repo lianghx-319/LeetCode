70fb815bdb6a4a84ddc65f35be182c61f48b3d53
Han <xsytby1112@gmail.com>
2020-05-29 11:30:38 +0800

    chore: modify variable name

--- 24.swap-nodes-in-pairs.go
+++ 24.swap-nodes-in-pairs.go
@@ -13,15 +13,15 @@
  * }
  */
 func swapPairs(head *ListNode) *ListNode {
-	fakeHead := &ListNode{Next: head}
-	pre := fakeHead
+	pHead := &ListNode{Next: head}
+	pre := pHead
 	for pre.Next != nil && pre.Next.Next != nil {
 		a := pre.Next
 		b := a.Next
 		pre.Next, b.Next, a.Next = b, a, b.Next
 		pre = a
 	}
-	return fakeHead.Next
+	return pHead.Next
 }
 
 // @lc code=end
